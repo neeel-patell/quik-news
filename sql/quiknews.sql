5c2aa31c020c0ebbdb7a5acde2e1d94a296d1b67
Neel Patel <neelpatel1905.np@gmail.com>
2020-07-29 09:25:01 +0530

    final sql file upload to github

---
+++ sql/quiknews.sql
@@ -0,0 +1,50 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.4.10-MariaDB : Database - quiknews
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`quiknews` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `quiknews`;
+
+/*Table structure for table `images` */
+
+CREATE TABLE `images` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `image` text NOT NULL,
+  `category` int(8) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `blog` */
+
+CREATE TABLE `blog` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `subject` char(40) NOT NULL,
+  `description` longtext NOT NULL,
+  `image_id` int(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `image_id` (`image_id`),
+  CONSTRAINT `image_id` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

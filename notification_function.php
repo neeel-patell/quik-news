29d34ed3be76240d52d6b8f1fa38cdf28a1ee6db
Neel Patel <neelpatel1905.np@gmail.com>
2020-08-24 18:31:21 +0530

    feature: notification function

---
+++ notification_function.php
@@ -0,0 +1,43 @@
+<?php
+    require_once 'connection.php';
+    function send_notification($title){
+        $conn = getConn();
+        $tokens = array();
+
+        // retrieving token of devices which have installed the app on their device
+        $result = $conn->query("SELECT token from android_devices");
+        while($row = $result->fetch_array()){
+            array_push($tokens,$row['token']);
+        }
+
+        // Simple message that shows that post has uploaded
+        $message = array("body" =>"New Post is just Uploaded in $title category" , "title"=>$title);
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        $fields = array(
+            'registration_ids' => $tokens,
+            'notification' => $message
+            );
+
+        $headers = array(
+            'Authorization:key = AAAADIiwSbI:APA91bHRPnaHqJfn9rYelU2-Mo8tl6w3LDymRuKq5_n_iHQJoFgSZ7qsWkxdxyF_acstBxRCZOTBlRYiTuMREmDPyWj1W5st7ObufB5E5CXJB2cNgepjiQ6vDEFPqOSFDVDgzBjXD4a9',
+            'Content-Type: application/json'
+            );
+
+    $ch = curl_init();
+
+    // setting http headers and variables to pass to send notification
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+    $result = curl_exec($ch);           
+    if ($result === FALSE) {
+        die('Curl failed: ' . curl_error($ch));
+    }
+    curl_close($ch);
+    return $result;
+    }
+?>
